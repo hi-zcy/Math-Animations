022b350874583d0ce4bc97a5b5636652a4ef424b
张小云 <160196654+hi-zcy@users.noreply.github.com>
2025-07-01 19:04:49 +0800

    更新 arc.intro.py

--- src/arc.intro.py
+++ src/arc.intro.py
@@ -41,7 +41,7 @@ class ARCIntro(ThreeDScene):
         code = Code(
             code_string="\n".join(code_lines),  # 修正这里
             tab_width=4,
-            line_numbers=False,
+            # line_numbers=False,
             style="monokai",
             font="Monospace",
             language="python",
